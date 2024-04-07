31b042fd69e498cfad203aa160f66d8bc89da5cb
Ershov90 <zp.ershov@gmail.com>
2024-04-07 22:05:38 +0300

    dz 1_2

--- dz1_2.c
+++ dz1_2.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    printf ("Let’s\n"); 
-    printf ("   go\n");
-    printf ("     to walk\n");
+    printf ("Let's\n"); 
+    printf ("  go\n");
+    printf ("    to walk\n");
 	
     return 0;
 }
