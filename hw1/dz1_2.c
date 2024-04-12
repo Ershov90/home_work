fb0a71ae36b195e31f5da2c459949a62b949015c
Ershov90 <zp.ershov@gmail.com>
2024-04-12 21:12:54 +0300

    replace

---
+++ hw1/dz1_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf ("Let's\n"); 
+    printf ("  go\n");
+    printf ("    to walk\n");
+	
+    return 0;
+}
+
+
---
+++ hw1/dz1_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf ("   *\n"); 
+	printf ("  ***\n");
+	printf (" *****\n");
+	printf ("*******\n"); 
+	printf (" HHOHH\n"); 
+	printf (" ZZZZZ\n"); 
+
+	return 0;
+}
+
+
