92f239b6f744d89fd93a8b68b7d456cc4efedd09
Ershov90 <zp.ershov@gmail.com>
2024-04-13 18:04:30 +0300

    hw3

---
+++ hw3/hw3_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b,c;
+    scanf ("%d%d%d",&a,&b,&c);
+    printf ("%.2f", (float)(a+b+c)/3);
+	return 0;
+}
+
---
+++ hw3/hw3_6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+    scanf ("%d",&a);
+    printf ("%d\n", (a%10)+((a/10)%10)+((a/100)%10));
+	return 0;
+}
